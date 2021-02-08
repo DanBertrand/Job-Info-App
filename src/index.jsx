819b3a5ac3ae13d1f97be980abc0fab040d61c97
Bertrand <dan.bertrand.hs@gmail.com>
2021-02-08 12:46:45 +0100

    First Commit

---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+// import './style.scss';
+import React from 'react';
+import ReactDOM from 'react-dom';
+ 
+const App = () => {
+  return (
+    <p>Hello World</p>
+  )
+};
+ 
+ReactDOM.render(<App />, document.getElementById('root'));
+ 
